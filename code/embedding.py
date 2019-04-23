361c7d4865b957d853756ec48664d2f92c621121
nicolas-kuechler <nico.kuechler@gmail.com>
2019-04-23 09:19:09 +0200

    adjust loading of embedding
    Former-commit-id: a856b2be5b1fe783b625286f7e15f7252b6080f4

--- code/embedding.py
+++ code/embedding.py
@@ -5,26 +5,40 @@ Code that loads the pretrained embedding
 # Standard packages
 import tensorflow as tf
 import numpy as np
-import pandas as pd 
+import pandas as pd
 from gensim import models
 
-def load_embedding(vocab, path, dim_embedding, vocab_size):
+def load_embedding(dim_embedding, vocab_size):
+
+    try:
+        emb = np.load(PATH_EMBEDDING_MATRIX)
+        assert emb.shape[0] == vocab_size
+        assert emb.shape[1] == dim_embedding
+        print(f"Using Cached Embedding Matrix: {PATH_EMBEDDING_MATRIX}")
+    except IOError: # no embedding matrix found
+        print(f"Creating New Embedding Matrix from External Embeddings: {PATH_EXTERNAL_EMBEDDING}")
+        emb = load_external_embedding(path=PATH_EXTERNAL_EMBEDDING, dim_embedding=dim_embedding, vocab_size=vocab_size)
+
+        np.save(PATH_EMBEDDING_MATRIX, emb)
+
+
+    return emb
+
+
+def load_external_embedding(path, dim_embedding, vocab_size):
     '''
-      vocab          A dictionary mapping token strings to vocabulary IDs
       emb            Embedding tensor of shape vocabulary_size x dim_embedding
       path           Path to embedding file
       dim_embedding  Dimensionality of the external embedding.
     '''
 
     print("Loading external embeddings from %s" % path)
-    
-    print(f'type of vocab: {type(vocab)}')
 
-    model = models.KeyedVectors.load_word2vec_format(path, binary=False)  
+    model = models.KeyedVectors.load_word2vec_format(path, binary=False)
     external_embedding = np.zeros(shape=(vocab_size, dim_embedding))
     matches = 0
-    
-    vocab = open('../data/vocab.txt', 'r')
+
+    vocab = open(PATH_VOCAB, 'r')
     idx = 0
     for tok in vocab:
 
@@ -41,16 +55,12 @@ def load_embedding(vocab, path, dim_embedding, vocab_size):
     external_embedding[idx] = np.random.uniform(low=-0.25, high=0.25, size=dim_embedding)
 
     print(f'index after loading embedding ==> {idx}')
-    
+
     print("%d words out of %d could be loaded" % (matches, vocab_size))
-    
+
     emb = external_embedding
 
     print('embedding type: ', type(emb))
     print(f'embedding shape: {emb.shape}')
-
-    np.save('../data/embedding_matrix.npy',emb)
-    #can be removed if np.save works
-    pd.DataFrame(emb).to_csv('../data/embedding_matrix.csv', header=False, index=False)
 
     return emb
--- code/global_variable.py
+++ code/global_variable.py
@@ -9,9 +9,10 @@ PATH_CONTINUATION = "../data/sentences/sentences.continuation"
 PATH_TEST = "../data/sentences/sentences_test.txt"
 PATH_VOCAB = "../data/vocab.txt"
 PATH_SUBMISSION = "../data/submission/"
-PATH_EMBEDDING = "../data/wordembeddings-dim100.word2vec"
 PATH_LOG = '../logs'
 PATH_CHECKPOINTS = '../tf_ckpts'
+PATH_EXTERNAL_EMBEDDING = "../data/wordembeddings-dim100.word2vec"
+PATH_EMBEDDING_MATRIX = "../data/embedding_matrix.npy"
 
 SPECIAL = {
     "bos" : "<bos>",
@@ -24,11 +25,6 @@ BATCH_SIZE = 64
 SHUFFLE_BUFFER_SIZE = 512
 VOCAB_SIZE = 20000
 
-
-WORD_EMBEDDINGS = np.array(pd.read_csv('../data/embedding_matrix.csv', header=None).values.tolist()) # <class 'numpy.ndarray'>
-
-
-# TODO load pretrained word embeddings and check that it's working in combination with tf.function
 EMBEDDING_SIZE = 100
 LSTM_HIDDEN_STATE_SIZE_A = 512
 LSTM_HIDDEN_STATE_SIZE_B = 1024
