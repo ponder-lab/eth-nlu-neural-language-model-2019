1cd5b836796cdcffa03a6896ee78296415b44097
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-06-19 07:43:46 -0400

    Enhance install script.

--- startup.sh
+++ startup.sh
@@ -2,8 +2,8 @@
 
 module load python_gpu/3.6.4 cuda/10.0.130 cudnn/7.5
 
-pip install --user pipenv
-virtualenv --system-site-packages -p python3 ./venv
+pip install --user pipenv || sudo apt install pipenv
+virtualenv -p python3 ./venv
 source ./venv/bin/activate
 pip3 install tf-nightly-gpu-2.0-preview
 pip3 install gensim
